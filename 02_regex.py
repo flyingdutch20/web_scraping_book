2669288f027e23423df3eb75e5b5bd98ed68ff3e
ted bracht <ted@bracht.uk>
2020-07-04 06:03:58 +0100

    Chapter 2 regex

--- 02_regex.py
+++ 02_regex.py
@@ -1,8 +1,10 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import re
 
 html = urlopen("http://www.pythonscraping.com/pages/page3.html")
 bsObj = BeautifulSoup(html)
 
-for child in bsObj.find("table", {"id":"giftList"}).tr.next_siblings:
-    print(child)
+images = bsObj.findAll("img", {"src":re.compile("\.\.\/img\/gifts/img.*\.jpg")})
+for image in images:
+    print(image["src"])
--- 09_logins_cookies.py
+++ 09_logins_cookies.py
@@ -1,10 +1,12 @@
 import requests
 
+session = requests.Session()
 
-params = {"username": "Ted", "password": "pwd"}
-r = requests.post("http://pythonscraping.com/pages/cookies/welcome.php", params)
+params = {"email": "sessions@wetherbyrunnersac.co.uk", "password": "TheMightyOrange"}
+r = requests.get("https://classfit.com/index.php/c/Login", auth=("sessions@wetherbyrunnersac.co.uk", "TheMightyOrange"))
 print("Cookie is set to:")
 print(r.cookies.get_dict())
 print("Going to profile page...")
-r = requests.get("http://pythonscraping.com/pages/cookies/profile.php", cookies=r.cookies)
+r = requests.get("https://classfit.com/index.php/c/MBOClasses?v=742", auth=("sessions@wetherbyrunnersac.co.uk", "TheMightyOrange"))
 print(r.text)
+
